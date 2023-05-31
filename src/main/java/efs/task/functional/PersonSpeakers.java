da13fc8c02952966e0dd7cf671d5364e90e1be61
Michal <pozdal.michal@gmail.com>
2023-05-31 12:55:27 +0200

    zrobione

--- src/main/java/efs/task/functional/PersonSpeakers.java
+++ src/main/java/efs/task/functional/PersonSpeakers.java
@@ -13,21 +13,29 @@ class PersonSpeakers {
         return p -> HELLO + p.getName();
     }
 
+    // TODO: stwórz odpowiednią implementację interfejsu Speaker, tak aby wywołanie jego metody say() powodowało
+    //  zwrócenie tekstu powitania zapisanego dużymi literami. Na zasadzie:
+    //      "HELLO. I'M MARIA.", gdzie Maria to imię danej osoby przekazanej w argumencie metody say().
+    //  Wywołanie metody say() interfejsu odbywa się w testach metody.
+    //  Aby być pewnym, że użyta jest prawidłowa pisownia zaleca się użycie odpowiedniej stałej.
     Speaker createShoutingSpeaker() {
-        // TODO: stwórz odpowiednią implementację interfejsu Speaker, tak aby wywołanie jego metody say() powodowało
-        //  zwrócenie tekstu powitania zapisanego dużymi literami. Na zasadzie:
-        //      "HELLO. I'M MARIA.", gdzie Maria to imię danej osoby przekazanej w argumencie metody say().
-        //  Wywołanie metody say() interfejsu odbywa się w testach metody.
-        //  Aby być pewnym, że użyta jest prawidłowa pisownia zaleca się użycie odpowiedniej stałej.
-        return null;
+        return p -> HELLO.toUpperCase() + p.getName().toUpperCase() + ".";
     }
 
+    // TODO: stwórz odpowiednią implementację interfejsu Speaker, tak aby wywołanie jego metody say() powodowało
+    //  zwrócenie tekstu powitania w odpowiednim języku. Przykładowo dla języka angielskiego:
+    //       "Hello. I'm Antonina.", gdzie Antonina to imię danej osoby przekazanej w argumencie metody say().
+    //  Wywołanie metody say() interfejsu odbywa się w testach metody.
+    //  Aby być pewnym, że użyta jest prawidłowa pisownia zaleca się użycie odpowiednich stałych.
     Speaker createGreetLocalSpeaker() {
-        // TODO: stwórz odpowiednią implementację interfejsu Speaker, tak aby wywołanie jego metody say() powodowało
-        //  zwrócenie tekstu powitania w odpowiednim języku. Przykładowo dla języka angielskiego:
-        //       "Hello. I'm Antonina.", gdzie Antonina to imię danej osoby przekazanej w argumencie metody say().
-        //  Wywołanie metody say() interfejsu odbywa się w testach metody.
-        //  Aby być pewnym, że użyta jest prawidłowa pisownia zaleca się użycie odpowiednich stałych.
-        return null;
+        return p -> {
+            if (p.getCountryOfLiving() == Country.PL) {
+                return CZESC + p.getName() + ".";
+            } else if (p.getCountryOfLiving() == Country.DE) {
+                return HALLO + p.getName() + ".";
+            } else {
+                return HELLO + p.getName() + ".";
+            }
+        };
     }
 }
